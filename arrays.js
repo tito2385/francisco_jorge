f83658d50a4447748f7052143544d09ff56a806a
tito2385 <98506663+tito2385@users.noreply.github.com>
2022-02-09 03:12:15 +0100

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,19 @@
+// Usos de Arrays en js
+
+let miArray = new Array(3)
+miArray[0]  = 'A'
+miArray[1]  = 100
+miArray[2]  = false
+
+console.log(miArray)
+
+miArray[3]  = false
+miArray[5]  = 'Mensaje'
+
+console.log(miArray)
+
+
+let miArray2 = []
+let miArray3 = ['🍇', '🍉', '🍍']
+console.log(miArray2)
+console.log(miArray3)
